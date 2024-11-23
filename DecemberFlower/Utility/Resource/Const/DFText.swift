38f511a27f08878c6c375ae7a4f8fc0bc691c20b
loveyMelon <manciepmc@gmail.com>
2024-11-24 00:13:10 +0900

    [Update] 새파일 대응

---
+++ DecemberFlower/Utility/Resource/Const/DFText.swift
@@ -0,0 +1,15 @@
+//
+//  DFText.swift
+//  DecemberFlower
+//
+//  Created by 김진수 on 11/23/24.
+//
+
+import Foundation
+
+enum DFText {
+    enum GiftDayText {
+        static let month = "12월이에요!"
+        static let purpose = "오늘의 목표를 날짜 박스에 넣어주세요"
+    }
+}
