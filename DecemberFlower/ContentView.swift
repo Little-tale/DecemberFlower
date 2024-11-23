6baf4a07a892508492c999ad2d90d7758f1043b7
Kim JaeHyung <you3192001@icloud.com>
2024-11-23 15:19:55 +0900

    Initial Commit

---
+++ DecemberFlower/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DecemberFlower
+//
+//  Created by Jae hyung Kim on 11/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DecemberFlower/DecemberFlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecemberFlowerApp.swift
+//  DecemberFlower
+//
+//  Created by Jae hyung Kim on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DecemberFlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
